f8a571c4e66b447bafc2b00d05a8d6f7eca8cce8
Danish <danishzayan6@gmail.com>
2022-08-03 12:23:08 +0530

    first comit

--- Lecture 3- If-Else, While loop & Lots of Patterns/sumofNumber.cpp
+++ Lecture 3- If-Else, While loop & Lots of Patterns/sumofNumber.cpp
@@ -4,15 +4,14 @@ using namespace std;
 int sumofNum(int a) {
     int sum = 0;
     for(int i = 1; i<=a; i++){
-        sum = sum + 1;
+        sum = sum + i;
     }
     cout << "1+2+3 " << sum;
 }
 
-int sumofNum()
 int main() {
     int n;
-    cout << "enter two no: ";
+    cout << "enter no: ";
     cin >> n;
     sumofNum(n);
 }
