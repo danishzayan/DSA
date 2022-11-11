3ab479cb53157779ab58d691caa7229919ad1de5
Danish <danishzayan6@gmail.com>
2022-11-11 22:21:35 +0530

    peak index in mountain arr

--- Lecture 13-/binarySearchOccurence.cpp
+++ Lecture 13-/binarySearchOccurence.cpp
@@ -2,56 +2,56 @@
 using namespace std;
 
 int firstOcc(int arr[], int size, int key) {
-  int start = 0, end = size - 1;
-  int mid = start + (end - start)/2;
+  int s = 0, e = size - 1;
+  int mid = s + (e - s)/2;
   int ans = -1;
 
-  while(start<=end) {
+  while(s<=e) {
     if(arr[mid] == key) {
       ans = mid;
-      end = mid - 1;
+      e = mid - 1;
     }
 
     else if(key > arr[mid]) {
-      start = mid + 1;
+      s = mid + 1;
     }
 
     else if(key < arr[mid]) {
-      end = mid - 1;
+      e = mid - 1;
     }
 
-    mid = start + (end - start)/2;
+    mid = s + (e - s)/2;
   }
   return ans;
 }
 
 int lastOcc(int arr[], int size, int key) {
-  int start = 0, end = size - 1;
-  int mid = start + (end - start)/2;
+  int s = 0, e = size - 1;
+  int mid = s + (e - s)/2;
   int ans = -1;
 
-  while(start<=end) {
+  while(s<=e) {
     if(arr[mid] == key) {
       ans = mid;
-      start = mid + 1;
+      s = mid + 1;
     }
 
     else if(key > arr[mid]) {
-      start = mid + 1;
+      s = mid + 1;
     }
 
     else if(key < arr[mid]) {
-      end = mid - 1;
+      e = mid - 1;
     }
 
-    mid = start + (end - start)/2;
+    mid = s + (e - s)/2;
   }
   return ans;
 }
 
 int main() {
   int even[10] = {2,4,6,6,8,8,8,8,8,9};
-  cout << "First occurence of 8 is " << firstOcc(even, 10, 8) << endl;
-  cout << "Last occurence of 8 is " << lastOcc(even, 10, 8) << endl;
+  cout << "First occurence of 8 is " << firstOcc(even, 10, 8) << el;
+  cout << "Last occurence of 8 is " << lastOcc(even, 10, 8) << el;
   return 0;
 }
